46251f580a8ddc01c6fc178a7d1e9d0cd8629005
Brenton @ Work <brenton@breakdesign.com.au>
2019-08-23 13:34:20 +1000

    Added minify support v1.06

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,8 @@
 
   //Use smooth scrolling when clicking on navigation
   $('.navbar-nav a').click(function() {
-    if (location.pathname.replace(/^\//,'') === 
-      this.pathname.replace(/^\//,'') && 
+    if (location.pathname.replace(/^\//,'') ===
+      this.pathname.replace(/^\//,'') &&
       location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
