677b1a65ef47c817a2a9e28d6e76b88cb9235eb2
Brenton <serenity@Brentons-iMac.local>
2020-03-01 16:00:32 +1100

    Installed NPM, Node.js, ESlint, Minify

--- js/scroll.js
+++ js/scroll.js
@@ -1,20 +1,24 @@
-    $(function() {
-  var topoffset =45; //variable for menu height
+$(function() {
+  var topoffset = 45; //variable for menu height
 
   //Use smooth scrolling when clicking on navigation
-  $('.navbar-nav a').click(function() {
-    if (location.pathname.replace(/^\//,'') ===
-      this.pathname.replace(/^\//,'') &&
-      location.hostname === this.hostname) {
+  $(".navbar-nav a").click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ===
+        this.pathname.replace(/^\//, "") &&
+      location.hostname === this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top-topoffset
-        }, 500);
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top - topoffset
+          },
+          500
+        );
         return false;
       } //target.length
     } //click function
   }); //smooth scrolling
-
 });
